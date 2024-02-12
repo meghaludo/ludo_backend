55ccdbeea44e29a5a60f0a77fddf66eb92139074
meghaludo <meghaludo@gmail.com>
2024-02-12 19:27:05 +0530

    test build

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,33 @@
 import { DataSource } from "typeorm";
 
+// export const AppDataSource = new DataSource({
+//   type: "mysql",
+//   host: "localhost",
+//   port: 3306,
+//   username: "root",
+//   password: "",
+//   // database: "ludo_game",
+//   database: "ludo_game",
+//   entities: ["src/entity/{*.ts, *.js}"],
+//   // entities: ["dist/entity/*.entity.js"],
+//   // entities: ["entity/*.entity.{ts,js}"],
+//   logging: true,
+//   synchronize: false,
+// });
+
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: "localhost",
   port: 3306,
-  username: "root",
-  password: "",
+  username: "megha",
+  password: "Megha@1234",
   // database: "ludo_game",
   database: "ludo_game",
   entities: ["src/entity/{*.ts, *.js}"],
   // entities: ["dist/entity/*.entity.js"],
   // entities: ["entity/*.entity.{ts,js}"],
   logging: true,
-  synchronize: false,
+  synchronize: true,
 });
 
 export default AppDataSource;
