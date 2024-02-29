d310b3ee38c7ddf3164a4abec946b41d9503d847
meghaludo <meghaludo@gmail.com>
2024-02-29 22:25:37 +0530

    upadte

--- dist/constants/message.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INTERNAL_SERVER_ERROR = void 0;
-exports.INTERNAL_SERVER_ERROR = 'Internal Server Error';
--- dist/routes/userCommonRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const UserCommonController_1 = require("../controllers/UserCommonController");
-const express_1 = require("express");
-const userCommonRoute = (0, express_1.Router)();
-const userCommonController = new UserCommonController_1.UserCommonController();
-// get contact-us page links
-userCommonRoute.get('/contact-us', userCommonController.getContactUsDetails);
-exports.default = userCommonRoute;
